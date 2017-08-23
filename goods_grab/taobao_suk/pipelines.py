7c5154bc7fa1a5a02d730f92030e7839f217aff5
xiao X <764191074@qq.com>
2017-08-23 11:03:39 +0800

    change log bug

--- goods_grab/taobao_suk/pipelines.py
+++ goods_grab/taobao_suk/pipelines.py
@@ -36,7 +36,7 @@ class TaobaoSukPipeline(object):
     def open_spider(self, spider):
         self.client = MongoClient(self.mongo_uri)
         self.db = self.client[self.mongo_db]
-        self.start_time = datetime.fromordinal(datetime.today().toordinal())
+        self.start_time = datetime.fromordinal(datetime.now().toordinal())
 
         for k, v in self.collection_name_list.items():
             for v_items in v:
@@ -59,7 +59,8 @@ class TaobaoSukPipeline(object):
     def close_spider(self, spider):
         num = self.db['goods_info'].find({'date': datetime.fromordinal(datetime.today().toordinal())}).count()
         shop_info_log = {'start': self.start_time,
-                         'end': datetime.fromordinal(datetime.today().toordinal()),
+                         'end': datetime.fromordinal(datetime.now().toordinal()),
+                         'date': datetime.fromordinal(datetime.today().toordinal()) ,
                          'count': num}
         self.db['goods_info_log'].insert(shop_info_log)
         self.client.close()
