7a096a1e07aaccf1dd771d79131b45f92222e8c6
764191074 <m13237118586@163.com>
2017-08-27 20:41:25 +0800

    add m

--- goods_grab/taobao_suk/middlewares.py
+++ goods_grab/taobao_suk/middlewares.py
@@ -155,16 +155,6 @@ class RetryMiddlewareDataIsNull(object):
             else:
                 raise IgnoreRequest()
 		
-        if response.status == 302 and 'login.taobao.com' in str(response.headers.get('Location', '')):
-            retry_return = _retry(self.max_retry_times, request, 'Data Is Null', spider)
-
-            change_ip(request)
-
-            if retry_return:
-                return retry_return
-            else:
-                raise IgnoreRequest()
-
         return response
 
 
