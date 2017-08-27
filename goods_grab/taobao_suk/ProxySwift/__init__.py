d13f22064cb15cfd06e5d9c1f80c6bb3d5d25de9
764191074 <m13237118586@163.com>
2017-08-27 20:24:18 +0800

    add  new  proxy

--- goods_grab/taobao_suk/ProxySwift/__init__.py
+++ goods_grab/taobao_suk/ProxySwift/__init__.py
@@ -13,20 +13,22 @@ import redis
 from threading import Thread
 '''
 使用说明：
-如果你选择手动配redis_url，请配置REDIS_URL，如果选择配到环境变量，请把REDIS_URL留空
 单例模式，请不要自行实例化类生产对象
 直接导入proxy_pool使用即可，目前兼容老接口change_ip（请使用新接口push_blacklist）和get
+启动代理池需要配置
+proxy_pool.redis_url = ''    # 远端redis_url,不填自动从环境变量获取
+proxy_pool.blacklist_clean_timing = 60 * 60 * 24   # 多长时间清理一次黑名单列表默认24小时清理一次
+proxy_pool.advance_time = 0 # ip存活预留时间 默认不填的话是10秒，不能大于ip的存活时间
+proxy_pool.start()     #  启动代理池   只启动一次不要重复启动，在所有配置完了之后启动就好
 函数说明：
 get函数随机获取一个ip
 例子：
 proxy_pool.get()
-push_blacklist和change_ip目前为同一个接口，请在新项目中使用push_blacklist，主要用于拉黑ip，把ip加入黑名单，拉黑后
-会自动通知主服务器，加快ip更换速度，并且再调用get函数的时候不会再出现拉黑的ip
+push_blacklist和change_ip目前为同一个接口，会自动通知主服务器，加快ip更换速度，并且再调用get函数的时候不会再出现拉黑的ip
+请在新项目中使用push_blacklist，主要用于拉黑ip，把ip加入黑名单，拉黑后
 例子：
 proxy_pool.push_blacklist('http://121.226.59.116:10000')
 '''
-REDIS_URL = 'redis://10.0.54.54:6378'
-setting_timing = 60*60*24
 root_path, file_name = os.path.split(os.path.realpath(__file__))
 
 
@@ -54,12 +56,14 @@ def init_logger(name, task_id, log_dir):
 class ProxyPool(object):
 
     # __start_time = datetime.datetime.now()
-    redis_url = REDIS_URL or os.environ.get('REDIS_URL', None)
     __available_ip_pool_key_name = 'available_ip_set'
     __death_ip_set_key_name = 'death_ip_set'
     __blacklist = set()
-
+    redis_url = None
+    blacklist_clean_timing = 60 * 60 * 24
+    advance_time = 0
     __pool = None
+    __pool_status = False
 
     def __init__(self):
 
@@ -69,11 +73,17 @@ class ProxyPool(object):
         self.logger.info(''.center(50, "*"))
         self.logger.info('proxy_pool is running'.center(50, "*"))
         self.logger.info(''.center(50, "*"))
-        assert self.redis_url is not None, 'redis_url is None'
+        self.__redis = None
+        self.__live_time = None
 
-        pool = redis.ConnectionPool.from_url(self.redis_url)
+    def start(self):
+        redis_url = self.redis_url or os.environ.get('REDIS_URL', None)
+        assert redis_url is not None, 'redis_url is None'
+
+        pool = redis.ConnectionPool.from_url(redis_url)
         self.__redis = redis.StrictRedis(connection_pool=pool)
         self.__live_time = int(str(self.__redis.get('live_time'), 'utf-8'))
+        assert self.advance_time <= self.__live_time, 'advance_time > ip_live_time'
 
         self.__get_all_ip()
         self.__start_refresh_task()
@@ -93,20 +103,21 @@ class ProxyPool(object):
         # 从 pool中随机取一个ip
         if _times >= 10:
             logging.info('retry times too many'.center(50, '*'))
-            time.sleep(60)
+            time.sleep(20)
 
         if self.__pool:
             ip_port = random.choice(self.__pool)
             now = datetime.datetime.now().timestamp()
-            if ip_port['last_time'] >= int(now) and ip_port['ip'] not in self.__blacklist:
+            if ip_port['last_time'] >= int(now) + self.advance_time and ip_port['ip'] not in self.__blacklist:
                 tmp_ip = "{0}:{1}".format(ip_port['ip'], ip_port['port'])
                 return tmp_ip
             else:
-                self.logger.debug('get(): ip in blacklist, or living is None, again'.format(ip_port))
+                self.logger.debug('get(): ip in blacklist, or living is None, again')
                 # self.__get_all_ip()
                 return self.get(_times=_times+1)
         else:
             time.sleep(0.1)
+            self.logger.debug('get(): __pool is None')
             self.__get_all_ip()
             return self.get(_times=_times+1)
 
@@ -157,7 +168,7 @@ class ProxyPool(object):
 
     def __timing_clean_blacklist_task(self):
         while True:
-            time.sleep(setting_timing)
+            time.sleep(self.blacklist_clean_timing)
             self.__blacklist = set()
 
     def __start_refresh_task(self):
@@ -171,7 +182,6 @@ class ProxyPool(object):
 
 
 proxy_pool = ProxyPool()
-
 '''
 import requests
 import time
