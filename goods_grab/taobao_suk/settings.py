2fbe41f1e21da8fae28469842f1680f4db400160
Administrator <764191074@qq.com>
2017-09-08 15:21:32 +0800

    add logs

--- goods_grab/taobao_suk/settings.py
+++ goods_grab/taobao_suk/settings.py
@@ -30,12 +30,12 @@ ROBOTSTXT_OBEY = False
 
 LOG_LEVEL = 'DEBUG'
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-#CONCURRENT_REQUESTS = 32
+CONCURRENT_REQUESTS = 32
 
 # Configure a delay for requests for the same website (default: 0)
 # See http://scrapy.readthedocs.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-DOWNLOAD_DELAY = 0.5
+DOWNLOAD_DELAY = 0.3
 # The download delay setting will honor only one of:
 CONCURRENT_REQUESTS_PER_DOMAIN = 5
 
