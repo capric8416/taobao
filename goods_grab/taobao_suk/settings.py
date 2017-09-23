d1170bbb1bd3b05ed9c360421b60a0d43d13de40
764191074 <m13237118586@163.com>
2017-09-23 13:26:36 +0800

    change deloy

--- goods_grab/taobao_suk/settings.py
+++ goods_grab/taobao_suk/settings.py
@@ -31,7 +31,8 @@ ROBOTSTXT_OBEY = False
 
 LOG_LEVEL = 'DEBUG'
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-CONCURRENT_REQUESTS = 12
+# CONCURRENT_REQUESTS = 12
+CONCURRENT_REQUESTS = 32
 
 # Configure a delay for requests for the same website (default: 0)
 # See http://scrapy.readthedocs.org/en/latest/topics/settings.html#download-delay
