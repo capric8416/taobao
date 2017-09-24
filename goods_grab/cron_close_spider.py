09684e339acd5ce1a559c61434034c213e38a11c
764191074 <m13237118586@163.com>
2017-09-24 11:27:49 +0800

    change delory

---
+++ goods_grab/cron_close_spider.py
@@ -0,0 +1,7 @@
+#! /usr/bin/env python3
+# -*- code: utf-8 -*-
+import redis
+import subprocess
+
+def close_spider():
+    subprocess.call(['super'])
--- goods_grab/taobao_suk/settings.py
+++ goods_grab/taobao_suk/settings.py
@@ -38,7 +38,7 @@ CONCURRENT_REQUESTS = 32
 # See http://scrapy.readthedocs.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
 # DOWNLOAD_DELAY = 0.7
-# DOWNLOAD_DELAY = 0.2
+DOWNLOAD_DELAY = 0.2
 
 # SCHEDULER_IDLE_BEFORE_CLOSE = 30
 # The download delay setting will honor only one of:
